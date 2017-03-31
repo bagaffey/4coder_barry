2f7023945e3d36706de7e8e1303a82f588440daa
Barry Gaffey <bagaffey@users.noreply.github.com>
2017-03-31 13:16:11 -0400

    Updated 4coder_barry.cpp

--- 4coder_barry.cpp
+++ 4coder_barry.cpp
@@ -5,3 +5,44 @@
 
 #include "4coder_default_include.cpp"
 #include "4coder_jump_parsing.cpp"
+
+#if !defined(Assert)
+#define Assert assert
+#endif
+
+#if !defined(internal)
+#define internal static
+#endif
+
+struct Parsed_Error
+{
+  int exists;
+  
+  String target_file_name;
+  int target_line_number;
+  int target_column_number;
+  
+  int source_buffer_id;
+  int source_position;
+};
+
+static bool GlobalEditMode;
+static char *GlobalCompilationBufferName = "*compilation*";
+
+static char BuildDirectory[4096] = "./";
+
+enum token_type
+{
+  Token_Unknown,
+  Token_OpenParen,
+  Token_CloseParen,
+  Token_Asterisk,
+  Token_Minus,
+  Token_Plus,
+  Token_ForwardSlash,
+  Token_Percent,
+  Token_Colon,
+  Token_Number,
+  Token_Comma,
+  Token_EndOfStream,
+};
