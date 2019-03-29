0ecc72c1b12a6bfcd56b8ad7aead13b93c9ec068
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-03-29 11:03:23 -0400

    Update 4coder_barry.cpp

--- 4coder_barry.cpp
+++ 4coder_barry.cpp
@@ -1516,7 +1516,7 @@ DEFINE_MODAL_KEY(modal_f, casey_paste_and_tab);
 DEFINE_MODAL_KEY(modal_g, goto_line);
 DEFINE_MODAL_KEY(modal_h, seek_white_or_token_left);
 DEFINE_MODAL_KEY(modal_i, move_up);
-DEFINE_MODAL_KEY(modal_j, seek_white_or_token_left);
+DEFINE_MODAL_KEY(modal_j, casey_list_all_functions_globally);
 DEFINE_MODAL_KEY(modal_k, move_down);
 DEFINE_MODAL_KEY(modal_l, seek_white_or_token_right);
 DEFINE_MODAL_KEY(modal_m, casey_save_and_make_without_asking);
