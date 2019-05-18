254ca0a66b96f44516a0b076c7f809862095095d
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-05-18 09:31:37 -0400

    Update 4coder_barry.cpp

--- 4coder_barry.cpp
+++ 4coder_barry.cpp
@@ -1727,7 +1727,6 @@ HOOK_SIG(casey_start)
         // { Stag_Cursor, },
         // { Stag_At_Cursor, },
         // { Stag_Cursor, },
-        // { Stag_At_Cursor, },
         { Stag_Comment, 0xff9800 },
         { Stag_Keyword, 0x5e98ba },
         { Stag_Preproc, 0x3f51b5 },
