be8a7b58cae49d7e863a87dc79ae8892aa37a0d2
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-05-23 19:21:25 -0400

    Update 4coder_barry.cpp

--- 4coder_barry.cpp
+++ 4coder_barry.cpp
@@ -1722,7 +1722,6 @@ HOOK_SIG(casey_start)
         // { Stag_Pop2, },
         // { Stag_Back, },
         // { Stag_Margin, },
-        // { Stag_Margin_Hover, },
         { Stag_Comment, 0xff9800 },
         { Stag_Keyword, 0x5e98ba },
         { Stag_Preproc, 0x3f51b5 },
